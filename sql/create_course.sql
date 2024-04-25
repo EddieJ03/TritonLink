fe8081155975109fa04c0f0827d488ea302744d7
Edward Jin <57547638+EddieJ03@users.noreply.github.com>
2024-04-25 11:49:20 -0700

    broken bash script

---
+++ sql/create_course.sql
@@ -0,0 +1,10 @@
+CREATE TABLE course (
+    course_number VARCHAR(50),
+    department VARCHAR(50),
+    min_unit INT,
+    max_unit INT,
+    letter_grade BOOLEAN,
+    S_or_U BOOLEAN,
+    lab_work BOOLEAN,
+    instructor_consent BOOLEAN
+);
