63df21256b79379eb18e2b90b4c2c2d426482eec
Edward Jin <57547638+EddieJ03@users.noreply.github.com>
2024-05-05 09:08:31 -0700

    updated create_attendance.sql

--- sql/create_attendance.sql
+++ sql/create_attendance.sql
@@ -1,6 +1,6 @@
 -- Define the attendance table
 CREATE TABLE attendance (
-    PID VARCHAR(10) REFERENCES student(PID),
+    PID VARCHAR(10) REFERENCES student(PID) ON DELETE CASCADE,
     start_date DATE,
     end_date DATE,
     PRIMARY KEY (PID, start_date)
