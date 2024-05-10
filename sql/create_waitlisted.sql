03ca2659b542b3ad57cec9f87097eaab5853efa7
Edward Jin <57547638+EddieJ03@users.noreply.github.com>
2024-05-10 10:06:49 -0700

    fix waitlisted

--- sql/create_waitlisted.sql
+++ sql/create_waitlisted.sql
@@ -1,7 +1,8 @@
 CREATE TABLE waitlisted (
     PID VARCHAR(10),
-    section_id VARCHAR(50),
-    PRIMARY KEY (PID, section_id),
+    section_id VARCHAR(20),
+    course_number VARCHAR(50) REFERENCES course(course_number) ON DELETE CASCADE,
+    PRIMARY KEY (PID, section_id, course_number),
     FOREIGN KEY (PID) REFERENCES student(PID) ON DELETE CASCADE,
-    FOREIGN KEY (section_id) REFERENCES classes(section_id) ON DELETE CASCADE
+    FOREIGN KEY (section_id, course_number) REFERENCES classes(section_id, course_number) ON DELETE CASCADE,
 );
