2db1fc8901f18377f66218b8467d1fd68a05b48e
Edward Jin <57547638+EddieJ03@users.noreply.github.com>
2024-05-10 09:59:51 -0700

    update review and weekly meeting schema

--- sql/create_review_meeting.sql
+++ sql/create_review_meeting.sql
@@ -1,8 +1,13 @@
--- Define the review_meeting table
+CREATE TYPE day_enum AS ENUM('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun');
+
 CREATE TABLE review_meeting (
-    section_id VARCHAR(20) REFERENCES classes(section_id) ON DELETE CASCADE,
-    start_time TIMESTAMP,
-    end_time TIMESTAMP,
+    section_id VARCHAR(20),
+    course_number VARCHAR(50) REFERENCES course(course_number) ON DELETE CASCADE,
+    date DATE,
+    day_of_week day_enum,
+    start_time TIME,
+    end_time TIME,
     location VARCHAR(100),
-    PRIMARY KEY (section_id, start_time)
+    FOREIGN KEY (section_id, course_number) REFERENCES classes(section_id, course_number) ON DELETE CASCADE,
+    PRIMARY KEY (section_id, course_number)
 );
--- sql/create_weekly_meeting.sql
+++ sql/create_weekly_meeting.sql
@@ -2,7 +2,7 @@ CREATE TYPE meeting_enum AS ENUM ('LE', 'DI', 'Lab Sessions');
 CREATE TYPE day_enum AS ENUM ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
 
 CREATE TABLE weekly_meeting (
-    section_id VARCHAR(20) REFERENCES classes(section_id) ON DELETE CASCADE,
+    section_id VARCHAR(20),
     course_number VARCHAR(50) REFERENCES course(course_number) ON DELETE CASCADE,
     meeting_type meeting_enum,
     start_time time,
@@ -11,5 +11,6 @@ CREATE TABLE weekly_meeting (
     end_date date,
     day_of_week day_enum,
     location VARCHAR(100),
+    FOREIGN KEY (section_id, course_number) REFERENCES classes(section_id, course_number) ON DELETE CASCADE,
     PRIMARY KEY (section_id, course_number, meeting_type, day_of_week)
 );
