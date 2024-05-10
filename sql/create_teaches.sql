5f2df453b7a8ad5978824a759cd7c38ebd797d7b
Edward Jin <57547638+EddieJ03@users.noreply.github.com>
2024-05-10 09:29:14 -0700

    another foreign key constraint teaches

--- sql/create_teaches.sql
+++ sql/create_teaches.sql
@@ -4,5 +4,6 @@ CREATE TABLE teaches (
     course_number VARCHAR(50),
     PRIMARY KEY (faculty_name, section_id, course_number),
     FOREIGN KEY (faculty_name) REFERENCES faculty(name) ON DELETE CASCADE,
+    FOREIGN KEY (course_number) REFERENCES course(course_number) ON DELETE CASCADE,
     FOREIGN KEY (section_id, course_number) REFERENCES classes(section_id, course_number) ON DELETE CASCADE
 );
