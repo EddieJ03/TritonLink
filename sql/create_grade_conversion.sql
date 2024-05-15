cead41102f66f7f15937a790b4c5fb8604380e7a
Edward Jin <57547638+EddieJ03@users.noreply.github.com>
2024-05-15 12:08:07 -0700

    add grade conversion table

---
+++ sql/create_grade_conversion.sql
@@ -0,0 +1,14 @@
+create table grade_conversion(
+    LETTER_GRADE CHAR(2) NOT NULL,
+    NUMBER_GRADE DECIMAL(2,1)
+);
+insert into grade_conversion values('A+', 4.3);
+insert into grade_conversion values('A', 4);
+insert into grade_conversion values('A-', 3.7);
+insert into grade_conversion values('B+', 3.4);
+insert into grade_conversion values('B', 3.1);
+insert into grade_conversion values('B-', 2.8);
+insert into grade_conversion values('C+', 2.5);
+insert into grade_conversion values('C', 2.2);
+insert into grade_conversion values('C-', 1.9);
+insert into grade_conversion values('D', 1.6);
