e9882c7dff2d56cf78b44dae68f431e3acbc57fe
Edward Jin <57547638+EddieJ03@users.noreply.github.com>
2024-05-23 16:08:44 -0700

    min_gpa can be 0

--- sql/create_category.sql
+++ sql/create_category.sql
@@ -3,7 +3,7 @@ CREATE TABLE category (
     category_id VARCHAR(20) PRIMARY KEY,
     degree_id VARCHAR(20),
     category_name VARCHAR(100),
-    min_gpa FLOAT NOT NULL CHECK (min_gpa > 0),
+    min_gpa FLOAT NOT NULL CHECK (min_gpa >= 0),
     required_units INTEGER NOT NULL CHECK (required_units > 0),
     is_concentration BOOLEAN,
     FOREIGN KEY (degree_id) REFERENCES degree(degree_id) ON DELETE CASCADE
