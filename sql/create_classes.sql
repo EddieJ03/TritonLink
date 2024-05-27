a5a8c318198b149445e7a530aa49e7a821320584
Edward Jin <57547638+EddieJ03@users.noreply.github.com>
2024-05-27 09:21:25 -0700

    cannot have lower enrollment limit than # of currently enrolled ppl

--- sql/create_classes.sql
+++ sql/create_classes.sql
@@ -10,3 +10,27 @@ CREATE TABLE classes (
     PRIMARY KEY (section_id, course_number),
     FOREIGN KEY (course_number) REFERENCES course(course_number) ON DELETE CASCADE
 );
+
+CREATE OR REPLACE FUNCTION check_new_enrollment_limit()
+RETURNS TRIGGER AS $$
+DECLARE
+    current_enrollment INT;
+BEGIN
+    SELECT COUNT(*)
+    INTO current_enrollment
+    FROM enrolled
+    WHERE enrolled.section_id = NEW.section_id
+      AND enrolled.course_number = NEW.course_number;
+
+    IF NEW.enrollment_limit < current_enrollment THEN
+        RAISE EXCEPTION 'New enrollment limit % cannot be less than current enrollment %', NEW.enrollment_limit, current_enrollment;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_check_new_enrollment_limit
+BEFORE UPDATE OF enrollment_limit ON classes
+FOR EACH ROW
+EXECUTE FUNCTION check_new_enrollment_limit();
